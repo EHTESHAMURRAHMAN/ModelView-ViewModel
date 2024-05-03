269655c18e19b0bf6947e8613b36a42e91d10011
EHTESHAMURRAHMAN <ehteshamkhan2002@gmail.com>
2024-05-03 18:18:41 +0530

    change title on main page

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Ehtesham',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
